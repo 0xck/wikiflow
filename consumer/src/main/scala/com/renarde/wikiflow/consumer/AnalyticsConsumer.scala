dcadbf92c0739fbe4df5d86583f7fd7518d70102
Constantine Kormashev <constantine.kormashev@gmail.com>
2019-08-16 09:44:33 +0300

    revert setings

--- consumer/src/main/scala/com/renarde/wikiflow/consumer/AnalyticsConsumer.scala
+++ consumer/src/main/scala/com/renarde/wikiflow/consumer/AnalyticsConsumer.scala
@@ -12,10 +12,10 @@ object AnalyticsConsumer extends App with LazyLogging {
 
   val spark: SparkSession = SparkSession.builder ()
     .appName (appName)
-    .config ("spark.driver.memory", "2g")
+    .config ("spark.driver.memory", "5g")
     .config ("spark.memory.offHeap.enabled", value = true)
-    .config ("spark.memory.offHeap.size", "4g")
-    .master ("local[1]")
+    .config ("spark.memory.offHeap.size", "7g")
+    .master ("local[2]")
     .getOrCreate
 
   import spark.implicits._
